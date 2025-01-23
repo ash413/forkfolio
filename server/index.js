3c4c1b68faba2eae3c46de2ee9a9b092f67aa159
ash413 <vaishnavi.kadam413@gmail.com>
2025-01-23 17:07:09 -0500

    okay deployed but connecting backend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const recipeRouter = require('./routes/recipeRoutes')
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://forkfolio-rho.vercel.app/',
     methods: ['POST', 'GET', 'UPDATE', 'DELETE'],
     credentials: true
 }));
